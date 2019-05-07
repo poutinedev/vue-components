dbeb1a02790ca912694aeb6038b46f15181726ec
Daryn St. Pierre <daryniscool@gmail.com>
2019-05-07 10:29:21 -0400

    Initial commit and first component.

---
+++ components/PrettyDate.vue
@@ -0,0 +1,22 @@
+<template>
+  <time :datetime="date" v-html="readableDate(date)"/>
+</template>
+
+<script>
+export default {
+  props: {
+    date: String
+  },
+  methods: {
+    readableDate(value) {
+      let newDate = new Date(value).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      })
+
+      return newDate
+    }
+  }
+}
+</script>
