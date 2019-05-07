c70ebcd53a1bdd60aa26aec7560f6e109e985f25
Daryn St. Pierre <daryniscool@gmail.com>
2019-05-07 10:44:42 -0400

    Placeholder image component added. Needs some error-checking.

---
+++ components/PlaceholderImage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img v-bind:src='fakeImage(600,600,"nature")' alt="" />
+    <p>Hard-refresh for a new image.</p>
+  </div>
+</template>
+
+<style lang="scss">
+:root {
+  text-align: center;
+  font: normal 18px/1.3 sans-serif;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
+
+<script>
+export default {
+  methods: {
+    fakeImage(w, h, cat) {
+      let c = cat != undefined ? cat : 'any'
+      let img = `https://placeimg.com/${w}/${h}/${c}`
+      return img
+    }
+  }
+}
+</script>
